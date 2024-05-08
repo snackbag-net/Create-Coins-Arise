721d26784d40b287319315b4b237755c60d09373
JXSnack <jx.snack@gmail.com>
2024-05-08 20:54:43 +0200

    add casting mold items

--- src/main/java/net/snackbag/coinsarise/item/ModItemGroups.java
+++ src/main/java/net/snackbag/coinsarise/item/ModItemGroups.java
@@ -15,6 +15,11 @@ public class ModItemGroups {
             FabricItemGroup.builder().displayName(Text.translatable("itemGroup.coins_arise"))
                     .icon(() -> new ItemStack(ModItems.SPUR_CASTING_MOLD)).entries((displayContext, entries) -> {
                         entries.add(ModItems.SPUR_CASTING_MOLD);
+                        entries.add(ModItems.BEVEL_CASTING_MOLD);
+                        entries.add(ModItems.SPROCKET_CASTING_MOLD);
+                        entries.add(ModItems.COG_CASTING_MOLD);
+                        entries.add(ModItems.CROWN_CASTING_MOLD);
+                        entries.add(ModItems.SUN_CASTING_MOLD);
                     })
                     .build());
 
--- src/main/java/net/snackbag/coinsarise/item/ModItems.java
+++ src/main/java/net/snackbag/coinsarise/item/ModItems.java
@@ -12,9 +12,19 @@ import net.snackbag.coinsarise.CreateCoinsArise;
 
 public class ModItems {
     public static final Item SPUR_CASTING_MOLD = registerItem("spur_casting_mold", new Item(new FabricItemSettings()));
+    public static final Item BEVEL_CASTING_MOLD = registerItem("bevel_casting_mold", new Item(new FabricItemSettings()));
+    public static final Item SPROCKET_CASTING_MOLD = registerItem("sprocket_casting_mold", new Item(new FabricItemSettings()));
+    public static final Item COG_CASTING_MOLD = registerItem("cog_casting_mold", new Item(new FabricItemSettings()));
+    public static final Item CROWN_CASTING_MOLD = registerItem("crown_casting_mold", new Item(new FabricItemSettings()));
+    public static final Item SUN_CASTING_MOLD = registerItem("sun_casting_mold", new Item(new FabricItemSettings()));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(SPUR_CASTING_MOLD);
+        entries.add(BEVEL_CASTING_MOLD);
+        entries.add(SPROCKET_CASTING_MOLD);
+        entries.add(COG_CASTING_MOLD);
+        entries.add(CROWN_CASTING_MOLD);
+        entries.add(SUN_CASTING_MOLD);
     }
 
     private static Item registerItem(String name, Item item) {
