10101db978151e3805c6450cfda5c339d2798220
JXSnack <jx.snack@gmail.com>
2024-05-08 19:17:55 +0200

    add base

---
+++ src/main/java/net/snackbag/coinsarise/CreateCoinsAriseClient.java
@@ -0,0 +1,10 @@
+package net.snackbag.coinsarise;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreateCoinsAriseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/snackbag/coinsarise/CreateCoinsAriseDataGenerator.java
@@ -0,0 +1,11 @@
+package net.snackbag.coinsarise;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreateCoinsAriseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
