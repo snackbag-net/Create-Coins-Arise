aa2655ff75e23405303e730f6c4ccdc5411a5aa8
JXSnack <jx.snack@gmail.com>
2024-05-09 13:10:10 +0200

    add block

---
+++ src/main/java/net/snackbag/coinsarise/block/BountyBoardBlock.java
@@ -0,0 +1,20 @@
+package net.snackbag.coinsarise.block;
+
+import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.Nullable;
+
+public class BountyBoardBlock extends BlockWithEntity implements BlockEntityProvider {
+    public BountyBoardBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return null;
+    }
+}
---
+++ src/main/java/net/snackbag/coinsarise/block/ModBlocks.java
@@ -0,0 +1,40 @@
+package net.snackbag.coinsarise.block;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.snackbag.coinsarise.CreateCoinsArise;
+
+public class ModBlocks {
+    public static final Block BOUNTY_BOARD_BLOCK = registerBlock("bounty_board",
+            new Block(FabricBlockSettings.create()
+                    .strength(0)
+                    .sounds(BlockSoundGroup.WOOD)
+                    .breakInstantly()
+                    .collidable(true)
+                    .hardness(0)
+                    .pistonBehavior(PistonBehavior.DESTROY)));
+
+    public static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(CreateCoinsArise.MOD_ID, name), block);
+    }
+
+    public static Item registerBlockItem(String name, Block block) {
+        return Registry.register(Registries.ITEM, new Identifier(CreateCoinsArise.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+    }
+
+    public static void registerBlocks() {
+        CreateCoinsArise.LOGGER.info("Registering Coins Arise blocks...");
+    }
+}
